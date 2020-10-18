ca6673379d86fe0e9d89d3d02af6a9e913f07b0b
beevk <beevk.singh@gmail.com>
2020-10-19 02:47:47 +0700

    feat: added card for displaying headlines

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card
+    elevation="2"
+    class="mx-auto my-12"
+    max-width="374"
+  >
+    <v-img
+      v-if="hasImage"
+      height="250"
+      lazy-src="https://via.placeholder.com/150"
+      :src="`${newsDetails.urlToImage}`"
+    />
+    <v-card-text class="imageFiller" v-else>
+      <div>
+        {{newsDetails.description}}
+      </div>
+    </v-card-text>
+    <v-card-title>
+      {{newsDetails.title}}
+    </v-card-title>
+    <v-card-subtitle v-if="hasAuthor">
+      - {{newsDetails.author}}
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+// import { NewsInterface } from '@/store/types';
+
+export default Vue.extend({
+  name: 'NewsCard',
+  props: ['newsDetails'],
+  data: () => ({
+  }),
+  computed: {
+    hasAuthor(): boolean {
+      return !!(this.newsDetails.author);
+    },
+    hasImage(): boolean {
+      return !!(this.newsDetails.urlToImage);
+    },
+  },
+});
+</script>
+
+<style scoped>
+  .imageFiller {
+    min-height: 250px;
+    display: flex;
+    align-items: center;
+    background-color: #1976d2;
+    color: #fff;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,88 @@
 <template>
   <div class="home">
       <h1>Headlines</h1>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="hasError">{{errorMessage}}</p>
       <div
-       v-for="topic in allNews"
-       :key="`${topic.url}-${topic.title}`"
-       @click="onClick(topic.publishedAt)"
+       class="cards"
+       v-else
+       v-for="topic in news"
+       :key="`${topic.url}`"
+       @click="onClick(topic.publishedAt, topic.title)"
       >
-        <p>{{topic.title}}</p>
+        <NewsCard :newsDetails="topic" />
       </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { mapGetters } from 'vuex';
-// import Header from '@/components/Header.vue'; // @ is an alias to /src
+import { mapGetters, mapActions } from 'vuex';
+// import { State, Action, Getter } from 'vuex-class';
+import NewsCard from '@/components/NewsCard.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    // Header,
+    NewsCard,
   },
   methods: {
-    onClick(url: string) {
-      this.$router.push({
-        name: 'newsDetails',
-        params: { name: url },
-      });
-    },
+    ...mapActions({
+      fetchNews: 'fetchNews',
+    }),
   },
-  computed: mapGetters(['allNews']),
+  computed: {
+    ...mapGetters({
+      news: 'allNews',
+      isLoading: 'isLoading',
+      hasError: 'hasError',
+      errorMessage: 'errorMessage',
+    }),
+  },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  // eslint-disable-next-line
+  fetchNews!: () => any
+
+  // computed: mapGetters(['allNews']);
+  // mapActions([fetchNews]);
+  // onClick(url: string) {
+  //   this.$router.push({
+  //     name: 'newsDetails',
+  //     params: { name: url },
+  //   });
+  // };
+  // get mapGetters({
+  //   allNews: 'allNews'
+  // });
+
+  // Move this method to SDK later
+  private sluggify = (publishedDate: string, name: string): string => {
+    const trimmedDate = publishedDate.trim().split('T')[0];
+    const title = name.toLowerCase()
+      .replace(/[^a-z0-9 -]/g, '')
+      .replace(/\s+/g, '-')
+      .replace(/-+/g, '-');
+    // return trimmedDate;
+    return `${trimmedDate}-${title}`;
+  }
+
+  onClick(publishedAt: string, title: string) {
+    const url = this.sluggify(publishedAt, title);
+    this.$router.push({
+      name: 'newsDetails',
+      params: { name: url },
+    });
+  }
+
+  created() {
+    this.fetchNews();
+  }
+}
 
 </script>
+
+<style scoped>
+  .cards {
+    display: flex;
+  }
+</style>
